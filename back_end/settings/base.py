f5e536395421aca3801caee0771f5a3f512dde5e
Daniel Capo <danielccapo@gmail.com>
2013-08-30 21:18:24 -0400

    prepend www added to base.py; make sure you add PREPEND_WWW = False to your local.py

--- back_end/settings/base.py
+++ back_end/settings/base.py
@@ -49,6 +49,8 @@ USE_L10N = True
 # If you set this to False, Django will not use timezone-aware datetimes.
 USE_TZ = True
 
+PREPEND_WWW = True
+
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/var/www/example.com/media/"
 MEDIA_ROOT = root("..", "uploads")
