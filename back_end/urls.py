247f4b8f2d858788809bf887124cbdf060c2c427
Daniel Capo <danielccapo@gmail.com>
2013-09-28 14:45:14 -0700

    prints

--- back_end/urls.py
+++ back_end/urls.py
@@ -4,22 +4,28 @@ from tastypie.api import Api
 from nox import api
 from . import settings
 
-v1_api = Api(api_name='v1')
-v1_api.register(api.CreateUserResource())
-v1_api.register(api.UserResource())
-v1_api.register(api.EventResource())
-v1_api.register(api.InviteResource())
-v1_api.register(api.TextPostResource())
-v1_api.register(api.ImagePostResource())
-v1_api.register(api.PlacePostResource())
-v1_api.register(api.PostResource())
-v1_api.register(api.PostCommentResource())
-v1_api.register(api.PostLikeResource())
-v1_api.register(api.PostDislikeResource())
+try:
+    print('start of urls')
+    v1_api = Api(api_name='v1')
+    v1_api.register(api.CreateUserResource())
+    v1_api.register(api.UserResource())
+    v1_api.register(api.EventResource())
+    v1_api.register(api.InviteResource())
+    v1_api.register(api.TextPostResource())
+    v1_api.register(api.ImagePostResource())
+    v1_api.register(api.PlacePostResource())
+    v1_api.register(api.PostResource())
+    v1_api.register(api.PostCommentResource())
+    v1_api.register(api.PostLikeResource())
+    v1_api.register(api.PostDislikeResource())
 
-urlpatterns = patterns('',
-    url(r'^$', TemplateView.as_view(template_name="index.html")),
-    url(r'^api/', include(v1_api.urls)),
-     # (r'^(?P<path>.*)$', 'django.views.static.serve', 
-        # {'document_root': settings.MEDIA_ROOT}),
-)
+    print ('through tastypie')
+    urlpatterns = patterns('',
+        url(r'^$', TemplateView.as_view(template_name="index.html")),
+        url(r'^api/', include(v1_api.urls)),
+         # (r'^(?P<path>.*)$', 'django.views.static.serve', 
+            # {'document_root': settings.MEDIA_ROOT}),
+    )
+    print('through patterns')
+except Exception as e:
+    print(e)
